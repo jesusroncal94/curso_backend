fb809bd3bb1103b1dc27d2cb1be6e56863bb1bb1
Jesus Roncal <jesusroncal94@gmail.com>
2019-09-02 20:54:29 -0500

    Tipos de datos, operadores matematicos y logicos

---
+++ 20190902_SEM04_CLA13/prueba.py
@@ -0,0 +1,40 @@
+monto1 = monto2 = monto3 = 300
+print('Monto 1: ', monto1, ', monto 2: ', monto2, ', monto 3: ', monto3 )
+
+titulo = 'Ruby'
+print('Variable titulo: ', titulo)
+titulo = 'Ruby on Rails'
+print('Variable titulo modificado: ', titulo)
+
+var1 = 30
+var2 = 200.5
+print('Variable 1: ', var1, ', variable 2: ', var2)
+
+var1 = 'Hello World'
+var2 = 'Python Programming'
+print("var1[0]: ", var1[0])
+print("var2[1:5]: ", var2[1:6])
+
+3 ** 2
+100 - 43
+100 % 10
+10 / 3
+5 // 2
+
+"""import sys
+sys.getsizeof(var2)"""
+
+5 < 4
+10 != 6
+6 == 9
+4 >= 3
+10 <= 5
+
+a = b = c = True
+d = False
+print(a and b and d)
+print(a and b and not d)
+print(not a and b and d)
+print(a or d)
+print(not d)
+print(a or d and not d)
